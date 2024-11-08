1394f31d319f55f373d84a669349c58c1f89809a
Toshiki Togo <108942850+Uzu83@users.noreply.github.com>
2024-11-08 10:24:59 +0900

    Initial Commit

---
+++ OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by 東郷翔士希 on 2024/11/08.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
